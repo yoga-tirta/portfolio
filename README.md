722c57d47bb89abd74ba98144775386d266461c8
Yoga Tirta Permana <yogatirtapermana552@gmail.com>
2025-07-08 10:21:40 +0700

    reworked new portfolio

---
+++ README.md
@@ -0,0 +1,84 @@
+<div align="center">
+
+![fortfolio showcase](https://user-images.githubusercontent.com/69642436/194177795-8e913eb5-46b9-4140-9409-4bc6c46e799e.png)
+
+# Personal Portfolio Website | [Demo🔗](https://somnathpaul.in)
+  
+### [Features](#features-) • [Tech Stack](#tech-stack-) • [Contents](#contents-) • [Contribute](#-how-to-contribute-) 
+
+![MIT License](https://img.shields.io/bower/l/mi)
+![Website Status](https://img.shields.io/website?url=https%3A%2F%2Fsomnathpaul.in)
+![Lines of code](https://sloc.xyz/github/sp\-xd/sp\-xd.github.io)
+![views](https://visitor-badge.laobi.icu/badge?page_id=sp\-xd/sp\-xd.github.io)
+
+</div>
+
+## Features 💡
+
+- 🎨 Theme Changer
+- 🏗️ Automatic Slideshow of Project Cards
+- 📰 Dynamic Blog Section using GraphQL API
+- 📱 Responsive across all devices
+- ✅ Valid HTML and CSS
+
+## Tech Stack 🚀
+
+- ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=flat&logo=css3&logoColor=white)
+- ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=flat&logo=html5&logoColor=white)
+- ![JavaScript](https://img.shields.io/badge/javascript-%23F7DF1E.svg?style=flat&logo=javascript&logoColor=%23323330)
+
+## Other Tools ✨
+
+- **ScrollReveal.js**
+- **Devicons**
+- **GoogleFonts** 
+- **Iconscout**
+- **Swiper.js**
+
+## Contents 📔
+```
+.
+├── 🏠 Home
+├── 💻 About 
+├── 🎯 Skills
+├── 🏗️ Projects
+├── 📰 Blogs/
+│   └── 📒 Personal Blogging Site
+├── 🚚 Services
+└── 📞 Contact
+```
+
+## ✨ How to Contribute ?
+
+> Please refer to contribution guidelines before submitting patches and additions.
+
+0. ⭐ Star this repo ;)
+1. 🔍 Find or Create an issue that you are interested in addressing or a feature that you would like to add.
+2. 🍴 Fork the repository associated with the issue to your local GitHub organization. This means that you will have a copy of the repository under ```your-GitHub-username/repository-name```.
+3. 🖱️Clone the repository to your local machine using ```git clone https://github.com/github-username/repository-name.git```.
+4. 🌿 Create a new branch for your fix using ```git checkout -b branch-name-here```.
+5. 🏗️ Make the appropriate changes for the issue you are trying to address or the feature that you want to add.
+6. ➕ Use ```git add insert-paths-of-changed-files-here``` to add the file contents of the changed files to the staging area.
+7. ✏️ Use ```git commit -m "Insert a short message of the changes made here"``` to store the contents of the index with a descriptive message.
+8. 📤 Push the changes to the remote repository using ```git push origin branch-name-here```.
+9. 📩 Submit a pull request to the upstream repository.
+10. 🗒️Title the pull request with a short description of the changes made and the issue or bug number associated with your change. For example, you can title an issue like so ```"Fix: Screen crashing after clicking X button"```.
+11. 🗒️In the description of the pull request, explain the changes that you made, any issues you think exist with the pull request you made, and any questions you have for the maintainer. It's OK if your pull request is not perfect (no pull request is), the reviewer will be able to help you fix any problems and improve it!
+12. ⏳ Wait for the pull request to be reviewed by a maintainer.
+13. 📝 Make changes to the pull request if the reviewing maintainer recommends them.
+14. 🥳🎉 Celebrate your success after your pull request is merged!
+
+NOTE: Be sure to merge the latest changes from "upstream" before making a pull request!
+
+## 👮 Contributions Guidelines
+
+Along with guidelines mentioned at  [hacktoberfest.com](https://hacktoberfest.com/participation/). <br>
+Here is some others: <br>
+
+1. Maintain readable code and add comments where necesssary
+2. Unnecessary Pull Request will marked with spam tag and will be not be merged.
+3. Commit with proper commit messages
+
+## License
+By contributing, you agree that your contributions will be licensed under its MIT License.
+
---
+++ main.js
@@ -0,0 +1,396 @@
+// import projectsData from "./assets/json/projects.json" assert { type: "json" };
+
+const reveal = {
+  distance: "30px",
+  easing: "ease-in",
+  duration: 500,
+  delay: 300,
+  origin: "top",
+};
+ScrollReveal().reveal(".main", reveal);
+
+/*---------------- MENU SHOW Y HIDDEN ----------------*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+const themesMenu = document.getElementById("themes-menu"),
+  themesToggle = document.getElementById("themes-menu-toggle"),
+  themesClose = document.getElementById("themes-close");
+
+/*---- MENU SHOW ----*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+    themesMenu.classList.remove("show-menu");
+  });
+}
+
+if (themesToggle) {
+  themesToggle.addEventListener("click", () => {
+    themesMenu.classList.add("show-menu");
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*---- MENU HIDDEN ----*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+if (themesClose) {
+  themesClose.addEventListener("click", () => {
+    themesMenu.classList.remove("show-menu");
+  });
+}
+
+/*---------------- REMOVE MENU MOBILE ----------------*/
+const navLink = document.querySelectorAll(".nav_link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  //when we click on each nav_link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*---------------- SKILLS ----------------*/
+const skillsContent = document.getElementsByClassName("skills_content"),
+  skillsHeader = document.querySelectorAll(".skills_header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills_content skills_close";
+  }
+  if (itemClass === "skills_content skills_close") {
+    this.parentNode.className = "skills_content skills_open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
+
+/*---------------- SERVICES MODAL ----------------*/
+
+/*-------------------------PROJECTS-------------------- */
+await fetch("./assets/json/projects.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((projectsData) => {
+    console.log(projectsData);
+    document.getElementById("project-cards").innerHTML = `
+         ${projectsData
+           .map(function (project) {
+             return `
+          <!-----------Project Card--------------->
+           <div class="project_card card swiper-slide" >
+          
+            <img class="project_image" src="${project.image}"/>
+          
+            <div class="project_data">
+              <h3 class="project_title">${project.title}</h3>
+          
+              <p class="project_description">
+                  ${project.description}
+              </p>
+          
+              <a href="${project.sourceLink}" target="_blank" class="button button-flex button-small project_button">Checkout<i class="uil uil-external-link-alt button_icon checkout_button_icon"></i></a>
+          
+            </div>
+          </div> 
+          `;
+           })
+           .join("")} 
+      `;
+  });
+
+/*-------------------- PROJECTS SWIPER  ----------------*/
+var swiper = new Swiper(".projects_content", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  centerSlide: true,
+  loop: true,
+  fade: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    568: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1366: {
+      slidesPerView: 5,
+    },
+  },
+});
+/*-------------------------- BLOGS ----------------------------*/
+function readingTime(text) {
+  const wpm = 225;
+  const words = text.trim().split(/\s+/).length;
+  const time = Math.ceil(words / wpm);
+  return time;
+}
+
+// Fetch function
+async function gql(query, variables = {}) {
+  const data = await fetch("https://gql.hashnode.com/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  return data.json();
+}
+
+//old query  query GetUserArticles($page: Int!) {
+//     user(username: "sp-xd") {
+//         publication {
+//             posts(page: $page) {
+//                 title
+//                 brief
+//                 slug
+//                 coverImage
+//                 dateAdded
+//                 contentMarkdown
+//             }
+//         }
+//     }
+// }
+
+// Query
+const GET_USER_ARTICLES = `
+    query GetUserArticles($numOfPosts: Int!) {
+      user(username: "sp-xd") {
+        publications(first: 1) {
+          edges {
+            node {
+              posts(first: $numOfPosts) {
+                edges {
+                  node {
+                    title
+                    brief
+                    slug
+                    coverImage {
+                      url
+                    }
+    								publishedAt
+                    readTimeInMinutes
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+}
+`;
+// making the call and injecting the articles
+gql(GET_USER_ARTICLES, { numOfPosts: 6 }).then((result) => {
+  const articles = result.data.user.publications.edges[0].node.posts.edges;
+  let container = document.createElement("div");
+  container.classList.add("blog_items");
+
+  articles.forEach((article) => {
+    article = article.node;
+    let blog_item = document.createElement("div");
+    blog_item.classList.add("blog_item", "card");
+
+    let blog_content = document.createElement("div");
+    blog_content.classList.add("blog_content");
+
+    let article_link = `https://blogs.somnathpaul.in/${article.slug}`;
+
+    let title = document.createElement("h3");
+    title.classList.add("blog_title");
+    let title_link = document.createElement("a");
+    title_link.target = "_blank";
+    title_link.href = article_link;
+    title_link.innerText = article.title;
+    title.appendChild(title_link);
+
+    let brief = document.createElement("p");
+    brief.classList.add("blog_brief");
+    brief.innerText = article.brief;
+
+    let read_more_link = document.createElement("a");
+    read_more_link.target = "_blank";
+    read_more_link.href = article_link;
+    read_more_link.classList.add("read_more_button"); //, 'button', 'button-flex');
+    read_more_link.innerHTML = `Read more <i class="uil uil-external-link-alt"></i>`;
+
+    let date = new Date(Date.parse(article.publishedAt));
+    let options = { day: "numeric", month: "long", year: "numeric" };
+    let date_added = document.createElement("span");
+    date_added.innerHTML = `<i class="uil uil-calender"></i> ${date.toLocaleString(
+      "en-US",
+      options
+    )} `;
+
+    let reading_time = document.createElement("span");
+    reading_time.innerHTML = `&nbsp <i class="uil uil-book-open"></i> ${article.readTimeInMinutes} min read`;
+
+    let meta_data = document.createElement("span");
+    meta_data.appendChild(date_added);
+    meta_data.appendChild(reading_time);
+
+    let coverImage = document.createElement("img");
+    coverImage.src = article.coverImage.url;
+    coverImage.classList.add("blog_img");
+
+    brief.appendChild(read_more_link);
+    blog_item.appendChild(coverImage);
+    blog_content.appendChild(title);
+    // blog_item.appendChild(link);
+    blog_content.appendChild(meta_data);
+    blog_content.appendChild(brief);
+    blog_item.appendChild(blog_content);
+    container.appendChild(blog_item);
+  });
+
+  document.querySelector(".blogs_container").appendChild(container);
+});
+
+/*---------------- SCROLL SECTIONS ACTIVE LINK ----------------*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    let sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
+/*---------------- CHANGE BACKGROUND HEADER ----------------*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+
+window.addEventListener("scroll", scrollHeader);
+
+/*---------------- SHOW SCROLL UP ----------------*/
+function scrollUp() {
+  const scrollUpButton = document.getElementById("scroll-up");
+
+  if (this.scrollY >= 560) scrollUpButton.classList.add("scroll-show");
+  else scrollUpButton.classList.remove("scroll-show");
+}
+
+window.addEventListener("scroll", scrollUp);
+
+/*---------------- DARK LIGHT themes ----------------*/
+const darkLightThemeButton = document.getElementById("dark_light-theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
+
+//  localStorage theme is being retrieved if the site was previously visited
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  darkLightThemeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  darkLightThemeButton.classList[
+    selectedIcon === "uil-moon" ? "add" : "remove"
+  ](iconTheme);
+}
+
+darkLightThemeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  darkLightThemeButton.classList.toggle(iconTheme);
+
+  // storing current theme to local storage
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+/*------------------- Color Themes --------------------- */
+const gradientThemesButtons = document.querySelectorAll(".theme_item");
+
+const selectedGradientThemeIndexNumber = localStorage.getItem(
+  "selectedGradientThemeIndexNumber"
+);
+
+if (selectedGradientThemeIndexNumber) {
+  gradientThemesButtons.forEach((el, id) => {
+    if (selectedGradientThemeIndexNumber == id) {
+      toggleColorTheme(el, id);
+    }
+  });
+}
+
+function toggleColorTheme(element, indexNumber) {
+  let itemId = element.id;
+
+  for (let i = 0; i < gradientThemesButtons.length; i++) {
+    gradientThemesButtons[i].classList.remove("theme-selected");
+    document.body.classList.remove("gradient-theme-" + (i + 1));
+  }
+
+  document.body.classList.add(itemId);
+  element.classList.add("theme-selected");
+
+  //storing current gradient theme to local storage
+  localStorage.setItem("selectedGradientThemeIndexNumber", indexNumber);
+}
+
+gradientThemesButtons.forEach((el, id) => {
+  el.addEventListener("click", () => {
+    toggleColorTheme(el, id);
+  });
+});
